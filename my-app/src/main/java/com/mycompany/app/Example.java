50df2d40c46e8e7c09b7193d3cb0e7a3497cca38
manar <manarahmedshehata@gmail.com>
2017-09-28 12:31:14 +0200

    Update Example.java

--- my-app/src/main/java/com/mycompany/app/Example.java
+++ my-app/src/main/java/com/mycompany/app/Example.java
@@ -14,7 +14,7 @@ public class Example
 {
    @RequestMapping("/")
     String home() {
-        return "Hello manar!";
+        return "Hello manar !";
     }
 
     public static void main(String[] args) throws Exception {
